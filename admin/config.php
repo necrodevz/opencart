2f341094c58dbd971b979cd86cf1bae18d6aa539
Devroy Blake <necrodevz@gmail.com>
2015-01-28 06:31:31 -0500

    initial setup complete

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://kappy-d.kapildevprojects.com/sites/projects/ecommerce/opencart/admin/');
+define('HTTP_CATALOG', 'http://kappy-d.kapildevprojects.com/sites/projects/ecommerce/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://kappy-d.kapildevprojects.com/sites/projects/ecommerce/opencart/admin/');
+define('HTTPS_CATALOG', 'http://kappy-d.kapildevprojects.com/sites/projects/ecommerce/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/sites/projects/ecommerce/opencart/admin/');
+define('DIR_SYSTEM', '/var/www/html/sites/projects/ecommerce/opencart/system/');
+define('DIR_LANGUAGE', '/var/www/html/sites/projects/ecommerce/opencart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/sites/projects/ecommerce/opencart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/sites/projects/ecommerce/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/html/sites/projects/ecommerce/opencart/image/');
+define('DIR_CACHE', '/var/www/html/sites/projects/ecommerce/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/sites/projects/ecommerce/opencart/system/download/');
+define('DIR_UPLOAD', '/var/www/html/sites/projects/ecommerce/opencart/system/upload/');
+define('DIR_LOGS', '/var/www/html/sites/projects/ecommerce/opencart/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/sites/projects/ecommerce/opencart/system/modification/');
+define('DIR_CATALOG', '/var/www/html/sites/projects/ecommerce/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://kappy-d.kapildevprojects.com/sites/projects/ecommerce/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://kappy-d.kapildevprojects.com/sites/projects/ecommerce/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/sites/projects/ecommerce/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/html/sites/projects/ecommerce/opencart/system/');
+define('DIR_LANGUAGE', '/var/www/html/sites/projects/ecommerce/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/sites/projects/ecommerce/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/sites/projects/ecommerce/opencart/system/config/');
+define('DIR_IMAGE', '/var/www/html/sites/projects/ecommerce/opencart/image/');
+define('DIR_CACHE', '/var/www/html/sites/projects/ecommerce/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/sites/projects/ecommerce/opencart/system/download/');
+define('DIR_UPLOAD', '/var/www/html/sites/projects/ecommerce/opencart/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/sites/projects/ecommerce/opencart/system/modification/');
+define('DIR_LOGS', '/var/www/html/sites/projects/ecommerce/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
